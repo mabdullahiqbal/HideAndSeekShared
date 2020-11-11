dfb65b10e289037f2077f24614a250b6aab36094
zarghamshahid07 <73646494+zarghamshahid07@users.noreply.github.com>
2020-11-11 20:02:28 +0500

    Map 1and toti puti movmnt

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float M_Sensitivity = 100f;
+    public Transform player;
+    public Vector3 offset;
+   
+   
+    void FixedUpdate()
+    {
+        float mov_x = Input.GetAxis("Mouse X") * M_Sensitivity * Time.deltaTime;
+        float mov_y = Input.GetAxis("Mouse Y") * M_Sensitivity * Time.deltaTime;
+        
+        transform.Rotate(Vector3.up * mov_x);
+
+    }
+
+        
+}
+
---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    private string movinput = "Vertical";
+    private string turninput = "Horizontal";
+    public float rotation_rate = 360;
+    public float mov_speed = 50;
+    private Rigidbody rb;
+    private CharacterController cc;
+
+    
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();
+        cc = GetComponent<CharacterController>();
+
+    }
+    void FixedUpdate()
+    {
+        float movAxis = Input.GetAxis(movinput);
+        float turnAxis = Input.GetAxis(turninput);
+         Apply_Input(movAxis,turnAxis);
+         
+
+    }
+    private void Apply_Input(float mov,float turn)
+    {
+        Move(mov);
+        Turn(turn);
+
+    }
+    private void Move(float input)
+    {
+        //transform.Translate(Vector3.forward * input * mov_speed);
+       // rb.AddForce(Vector3.forward * input * mov_speed,ForceMode.VelocityChange);
+       cc.Move(Vector3.forward * input * mov_speed);
+
+    }
+    private void Turn(float input)
+    {
+        
+       // transform.Translate(Vector3.right * input * mov_speed);
+       //rb.AddForce(Vector3.right * input * mov_speed,ForceMode.VelocityChange);
+       cc.Move(Vector3.right * input * mov_speed);
+
+    }
+}
